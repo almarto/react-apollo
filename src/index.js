8f608b61deb8c0fba6d50f1a828147fba851d280
alberto.martinez <alberto.martinez@edorasware.com>
2018-09-29 18:17:43 +0200

    Prepare code to continue tutorial:
      * Clean unused testing code
      * Move devDependencies to devDependencies instead of dependencies where they were
      * Add and configure flow
      * Configure babel
      * Add husky
      * Add lint-staged
      * Configure lint-staged to run eslint and prettier
      * Configure husky to trigger lint-staged

--- src/index.js
+++ src/index.js
@@ -1,27 +1,24 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import { ApolloProvider } from "react-apollo";
-import { ApolloClient } from "apollo-client";
-import { createHttpLink } from "apollo-link-http";
-import { InMemoryCache } from "apollo-cache-inmemory";
+import {ApolloProvider} from 'react-apollo';
+// import {ApolloClient} from 'apollo-client';
+// import {createHttpLink} from 'apollo-link-http';
+// import {InMemoryCache} from 'apollo-cache-inmemory';
 
-import "./index.css";
-import App from "./components/App";
-import registerServiceWorker from "./registerServiceWorker";
+import './index.css';
+import App from './components/App';
 
-const httpLink = createHttpLink({ uri: "http://localhost:4000" });
+// const httpLink = createHttpLink({uri: 'http://localhost:4000'});
 
-const client = new ApolloClient({
-  link: HttpLink,
-  cache: new InMemoryCache()
-});
+// const client = new ApolloClient({
+//   link: HttpLink,
+//   cache: new InMemoryCache()
+// });
 
 ReactDOM.render(
   <ApolloProvider>
     <App />
   </ApolloProvider>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
-
-registerServiceWorker();
