47a9ef5e03ff565e6a3ee0a20b50e95f73c46e0c
alberto.martinez <alberto.martinez@edorasware.com>
2018-09-29 15:26:08 +0200

    Tutorial First Step:
      * Create react app
      * Improve project structure (Move app to components folder)
      * Add apollo-server with prisma following tutorial

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient } from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+import "./index.css";
+import App from "./components/App";
+import registerServiceWorker from "./registerServiceWorker";
+
+const httpLink = createHttpLink({ uri: "http://localhost:4000" });
+
+const client = new ApolloClient({
+  link: HttpLink,
+  cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+  <ApolloProvider>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
+
+registerServiceWorker();
